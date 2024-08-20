e026d6c9b6b8d3f3d88f3ac9624b73425b7a15d3
Chris-Miracle <chrisstarkmira15@gmail.com>
2024-08-20 20:24:55 +0100

    types and structs

--- main.go
+++ main.go
@@ -1,20 +1,24 @@
 package main
 
-import "log"
+import (
+	"log"
+	"time"
+)
 
-
-func main() {
-	var color string
-	color = "Green"
-
-	log.Println("Color is", color)
-
-	changeColor(&color)
-
-	log.Println("New Color is", color)
+type User struct {
+	FirstName   string
+	LastName    string
+	PhoneNumber string
+	Age         int
+	BirthDate   time.Time
 }
 
-func changeColor(s *string) {
-	newColor := "Blue"
-	*s = newColor
+func main() {
+	user := User{
+		FirstName:   "Miracle",
+		LastName:    "Chris",
+		PhoneNumber: "0700000000",
+	}
+
+	log.Println(user.FirstName, user.LastName, user.PhoneNumber)
 }
