842cca74b1f564cec345dc898b5c10bd52f58602
Chris-Miracle <chrisstarkmira15@gmail.com>
2024-08-22 21:38:55 +0100

    feat: create new pages with functions & handlers

--- main.go
+++ main.go
@@ -5,18 +5,32 @@ import (
 	"net/http"
 )
 
+const portNumber = ":4545"
+
+// Home is the handler for the home page
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the home page")
+}
+
+// About is the handler for the about page
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	fmt.Fprintf(w, "This is the about page, the sum is %d", sum)
+}
+
+// addValues adds two values and returns the result
+func addValues(x, y int) int {
+	return  x + y
+}
+
+
+// main is the entry point for the application
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello, World!")
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
-		if err != nil {
-			fmt.Println(err)
-		}
 
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-	})
-
-	fmt.Println("Listening on port 4545")
-	_  = http.ListenAndServe(":4545", nil)
+	fmt.Printf("Listening on localhost%s \n", portNumber)
+	_ = http.ListenAndServe(portNumber, nil)
 }
