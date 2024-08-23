1406b0394673e941b04f6e0b73993bd60934da72
Chris-Miracle <chrisstarkmira15@gmail.com>
2024-08-23 13:57:11 +0100

    feat: manage layouts using templates

--- pgk/render/render.go
+++ pgk/render/render.go
@@ -8,7 +8,7 @@ import (
 
 // RenderTemplate renders a template
 func RenderTemplate(w http.ResponseWriter, templateName string) {
-	parsedTenplate, _ := template.ParseFiles("./templates/" + templateName)
+	parsedTenplate, _ := template.ParseFiles("./templates/" + templateName, "./templates/base.layout.tmpl")
 	err := parsedTenplate.Execute(w, nil)
 	if err != nil {
 		fmt.Println("Error parsing template:", err)
