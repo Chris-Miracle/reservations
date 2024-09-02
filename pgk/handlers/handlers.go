cebc5ce77acc15e0eadb0a9d49cc70f6227dff8d
Chris-Miracle <chrisstarkmira15@gmail.com>
2024-09-02 22:05:30 +0100

    feat: trying sessions

--- pgk/handlers/handlers.go
+++ pgk/handlers/handlers.go
@@ -27,6 +27,8 @@ func NewHandlers(repository *Repository) {
 
 // Home is the handler for the home page
 func (repository *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIp := r.RemoteAddr
+	repository.App.Session.Put(r.Context(), "remote_ip", remoteIp)
 	render.RenderTemplate(w, "home.page.tmpl", &models.TemplateData{})
 }
 
@@ -34,6 +36,10 @@ func (repository *Repository) Home(w http.ResponseWriter, r *http.Request) {
 func (repository *Repository) About(w http.ResponseWriter, r *http.Request) {
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hi Chris."
+
+	remoteIP := repository.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
+
 	render.RenderTemplate(w, "about.page.tmpl", &models.TemplateData{
 		StringMap: stringMap,
 	})
