b8e8e7b7697815537dd924f196df7be235c6269d
Chris-Miracle <chrisstarkmira15@gmail.com>
2024-12-13 12:33:21 +0100

    test: update render & handler setup tests

--- internal/handlers/setup_test.go
+++ internal/handlers/setup_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"text/template"
 	"time"
@@ -27,6 +28,12 @@ func getRoutes() http.Handler {
 	gob.Register(models.Reservation{})
 
 	app.InProduction = false
+
+	infoLog := log.New(os.Stdout,  "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
 
 	//  Sessiion Props
 	session = scs.New()
